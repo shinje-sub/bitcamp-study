6470467ed651a0fb980079c78bc24139f51bc8eb
Shin je sub <wltjq2006@naver.com>
2019-12-14 17:36:16 +0900

    2019/12/14 Study

---
+++ my-project/src/main/java/App.java
@@ -0,0 +1,12 @@
+// 구구단 출력
+public class App {
+ 
+  public static void main(String[] args) {
+    {  
+      for(int i=1; i<10; i++)
+        for (int k=1; k<10; k++)
+        System.out.printf("%d * %d = %d\n", k, i, 2*i);
+    }
+    
+}
+}
