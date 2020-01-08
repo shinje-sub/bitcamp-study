bf8efc3ad062b74b9b1cd736bcb6f435befb2ee9
shinji-sub <wltjq2006@naver.com>
2020-01-08 16:59:57 +0900

    2020-01-08 v19_1 CRUD

--- bitcamp-project/src/main/java/com/eomcs/lms/util/ArrayList2.java
+++ bitcamp-project/src/main/java/com/eomcs/lms/util/ArrayList2.java
@@ -2,32 +2,44 @@ package com.eomcs.lms.util;
 
 import java.util.Arrays;
 
-public class ArrayList<E> {
-  
-  private static final int DEFAULT_CAPACITY = 2;
-  
+public class ArrayList2<E> {
+
+  private static final int DEFAULT_CAPACITY = 2; 
+
   Object[] elementData;
   int size;
-  
-  public ArrayList() {
+
+  public ArrayList2() {
     this.elementData = new Object[DEFAULT_CAPACITY];
   }
-  
-  public ArrayList(int initialCapacity) {
+
+  public ArrayList2(int initialCapacity) {
     if (initialCapacity < DEFAULT_CAPACITY) {
       this.elementData = new Object[DEFAULT_CAPACITY];
     } else {
       this.elementData = new Object[initialCapacity];
     }
   }
-  
+
   public void add(E e) {
     if (this.size == this.elementData.length) {
-      grow();
+      int oldSize = this.elementData.length;
+      int newSize = oldSize + (oldSize >> 1);
+
+      this.elementData = Arrays.copyOf(this.elementData, newSize);
+
+      /*
+      Object[] newArr = new Object[newSize];
+
+      for (int i = 0; i < this.size; i++) {
+        newArr[i] = this.elementData[i];
+      }
+      this.elementData = newArr;
+       */
     }
     this.elementData[this.size++] = e;
   }
-  
+
   @SuppressWarnings("unchecked")
   public E get(int index) {
     if (index < 0 || index >= this.size) {
@@ -35,17 +47,17 @@ public class ArrayList<E> {
     }
     return (E) this.elementData[index];
   }
-  
+
   @SuppressWarnings("unchecked")
   public E set(int index, E e) {
     if (index < 0 || index >= this.size) {
       return null;
     }
-    E oldValue = (E) this.elementData[index];
+    E oldValue =(E) this.elementData[index];
     this.elementData[index] = e;
     return oldValue;
   }
-  
+
   @SuppressWarnings("unchecked")
   public E remove(int index) {
     if (index < 0 || index >= this.size) {
@@ -54,7 +66,7 @@ public class ArrayList<E> {
     
     E oldValue = (E) this.elementData[index];
     System.arraycopy(this.elementData, index + 1, 
-        this.elementData, index, this.size - (index + 1) );
+        this.elementData, index, this.size - (index + 1));
     /*
     for (int i = index + 1; i < this.size; i++) {
       this.elementData[i - 1] = this.elementData[i];
@@ -63,7 +75,7 @@ public class ArrayList<E> {
     this.size--;
     return oldValue;
   }
-  
+ 
   public int size() {
     return this.size;
   }
@@ -76,46 +88,14 @@ public class ArrayList<E> {
       arr[i] = this.elementData[i];
     }
     return arr;
-    */
+     */
   }
-  
   @SuppressWarnings("unchecked")
   public E[] toArray(E[] arr) {
     if (arr.length < this.size) {
-      // 파라미터로 받은 배열이 작을 때는 새 배열을 만들어 리턴.
-      return (E[]) Arrays.copyOf(this.elementData, this.size, arr.getClass());
+    return (E[])Arrays.copyOf(this.elementData, this.size, arr.getClass());
     }
     System.arraycopy(this.elementData, 0, arr, 0, this.size);
-    return arr; // 넉넉할 때는 파라미터로 받은 배열을 그대로 리턴. 
-  }
-  
-  public void add(int index, E value) {
-    if (index < 0 || index >= this.size)
-      return;
-
-    if (this.size == this.elementData.length) {
-      grow();
-    }
-
-    for (int i = size - 1; i >= index; i--)
-      this.elementData[i + 1] = this.elementData[i];
-
-    this.elementData[index] = value;
-    this.size++;
-  }
-  
-  private Object[] grow() {
-    return this.elementData = Arrays.copyOf(this.elementData, 
-        newCapacity());
-  }
-  
-  private int newCapacity() {
-    int oldSize = this.elementData.length;
-    return oldSize + (oldSize >> 1);
+    return arr;
   }
 }
-
-
-
-
-
