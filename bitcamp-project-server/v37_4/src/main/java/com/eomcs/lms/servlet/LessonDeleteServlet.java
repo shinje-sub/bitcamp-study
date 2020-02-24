2022771caaf9bd185092d5ba2c3e434020079a8e
shinji-sub <wltjq2006@naver.com>
2020-02-24 11:00:28 +0900

    v37_4 search 기능 추가

---
+++ bitcamp-project-server/v37_4/src/main/java/com/eomcs/lms/servlet/LessonDeleteServlet.java
@@ -0,0 +1,35 @@
+package com.eomcs.lms.servlet;
+
+import java.io.PrintStream;
+import java.util.Scanner;
+import com.eomcs.lms.dao.LessonDao;
+
+public class LessonDeleteServlet implements Servlet {
+
+  // DAO 클래스를 구체적으로 지정하기 보다는
+  // 인터페이스를 지정함으로써
+  // 향후 다른 구현체로 교체하기 쉽도록 한다.
+  //
+  LessonDao lessonDao;
+
+  public LessonDeleteServlet(LessonDao lessonDao) {
+    this.lessonDao = lessonDao;
+  }
+
+  @Override
+  public void service(Scanner in, PrintStream out) throws Exception {
+
+    out.println("번호? ");
+    out.println("!{}!");
+    out.flush();
+    int no = Integer.parseInt(in.nextLine());
+
+
+    if (lessonDao.delete(no) > 0) {
+      out.println("강의를 삭제했습니다.");
+
+    } else {
+      out.println("해당 번호의 강의가 없습니다.");
+    }
+  }
+}
