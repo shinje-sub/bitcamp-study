c09240d9c4b219fa99a608326e85dd167d8cf1c5
shinji-sub <wltjq2006@naver.com>
2020-02-25 16:35:21 +0900

    v38_3 코드 리팩토링하기 Prompt 추가

---
+++ bitcamp-project-server/v38_3/src/main/java/com/eomcs/lms/servlet/PhotoBoardDeleteServlet.java
@@ -0,0 +1,36 @@
+package com.eomcs.lms.servlet;
+
+import java.io.PrintStream;
+import java.util.Scanner;
+import com.eomcs.lms.dao.PhotoBoardDao;
+import com.eomcs.lms.dao.PhotoFileDao;
+import com.eomcs.util.Prompt;
+
+public class PhotoBoardDeleteServlet implements Servlet {
+
+  PhotoBoardDao photoBoardDao;
+  PhotoFileDao photoFileDao;
+
+  public PhotoBoardDeleteServlet(PhotoBoardDao photoBoardDao, PhotoFileDao photoFileDao) {
+    this.photoBoardDao = photoBoardDao;
+    this.photoFileDao = photoFileDao;
+  }
+
+
+  @Override
+  public void service(Scanner in, PrintStream out) throws Exception {
+
+    int no = Prompt.getInt(in, out, "번호? ");
+
+    // 사진 게시글을 삭제하기 전에 먼저 자식 테이블에 있는
+    // 첨부 파일 데이터를 삭제한다.
+    photoFileDao.deleteAll(no);
+
+    if (photoBoardDao.delete(no) > 0) {
+      out.println("사진 게시글을 삭제했습니다.");
+
+    } else {
+      out.println("해당 번호의 사진 게시글이 없습니다.");
+    }
+  }
+}
