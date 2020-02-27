5274f984435fceb0caadc15c16878a05d9dbf859
shinji-sub <wltjq2006@naver.com>
2020-02-27 11:20:40 +0900

    v40_4 트랜잭션 관리자 도입하기

---
+++ bitcamp-project-server/v40_4/src/main/java/com/eomcs/lms/servlet/MemberUpdateServlet.java
@@ -0,0 +1,51 @@
+package com.eomcs.lms.servlet;
+
+import java.io.PrintStream;
+import java.util.Scanner;
+import com.eomcs.lms.dao.MemberDao;
+import com.eomcs.lms.domain.Member;
+import com.eomcs.util.Prompt;
+
+public class MemberUpdateServlet implements Servlet {
+
+  // DAO 클래스를 구체적으로 지정하기 보다는
+  // 인터페이스를 지정함으로써
+  // 향후 다른 구현체로 교체하기 쉽도록 한다.
+  //
+  MemberDao memberDao;
+
+  public MemberUpdateServlet(MemberDao memberDao) {
+    this.memberDao = memberDao;
+  }
+
+  @Override
+  public void service(Scanner in, PrintStream out) throws Exception {
+
+    int no = Prompt.getInt(in, out, "번호? ");
+
+    Member old = memberDao.findByNo(no);
+    if (old == null) {
+      out.println("해당 번호의 회원이 없습니다.");
+      return;
+    }
+    Member member = new Member();
+    member.setNo(no);
+
+    member.setName(Prompt.getString(in, out, "이름(%s)? "));
+
+    member.setEmail(Prompt.getString(in, out, "이메일(%s)? "));
+
+    member.setPassword(Prompt.getString(in, out, "암호(%s)? "));
+
+    member.setPhoto(Prompt.getString(in, out, "사진(%s)? "));
+
+    member.setTel(Prompt.getString(in, out, "전화번호(%s)? "));
+
+    if (memberDao.update(member) > 0) {
+      out.println("회원을 변경했습니다.");
+
+    } else {
+      out.println("변경에 실패했습니다.");
+    }
+  }
+}
