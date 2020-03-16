52ca54aa9bd1bd361017e420b63b991152295666
shinji-sub <wltjq2006@naver.com>
2020-03-16 17:16:37 +0900

    v46_1 객체 생성을 자동화하는 IoC 컨테이너 만들기

---
+++ bitcamp-project-server/v46_1/src/main/java/com/eomcs/lms/servlet/LessonAddServlet.java
@@ -0,0 +1,36 @@
+package com.eomcs.lms.servlet;
+
+import java.io.PrintStream;
+import java.util.Scanner;
+import com.eomcs.lms.domain.Lesson;
+import com.eomcs.lms.service.LessonService;
+import com.eomcs.util.Component;
+import com.eomcs.util.Prompt;
+
+@Component("/lesson/add")
+public class LessonAddServlet implements Servlet {
+
+  // DAO 클래스를 구체적으로 지정하기 보다는
+  // 인터페이스를 지정함으로써
+  // 향후 다른 구현체로 교체하기 쉽도록 한다.
+  //
+  LessonService lessonService;
+
+  public LessonAddServlet(LessonService lessonService) {
+    this.lessonService = lessonService;
+  }
+
+  @Override
+  public void service(Scanner in, PrintStream out) throws Exception {
+    Lesson lesson = new Lesson();
+
+    lesson.setTitle(Prompt.getString(in, out, "강의명? "));
+    lesson.setDescription(Prompt.getString(in, out, "내용? "));
+    lesson.setStartDate(Prompt.getDate(in, out, "강의 시작일? "));
+    lesson.setEndDate(Prompt.getDate(in, out, "강의 종료일? "));
+    lesson.setTotalHours(Prompt.getInt(in, out, "총 강의 시간? "));
+    lesson.setDayHours(Prompt.getInt(in, out, "일 수업 시간? "));
+    lessonService.add(lesson);
+    out.println("새 게시글을 등록했습니다.");
+  }
+}
---
+++ bitcamp-project-server/v46_1/src/main/java/com/eomcs/lms/servlet/MemberAddServlet.java
@@ -0,0 +1,34 @@
+package com.eomcs.lms.servlet;
+
+import java.io.PrintStream;
+import java.util.Scanner;
+import com.eomcs.lms.domain.Member;
+import com.eomcs.lms.service.MemberService;
+import com.eomcs.util.Component;
+import com.eomcs.util.Prompt;
+
+@Component("/member/add")
+public class MemberAddServlet implements Servlet {
+
+  // DAO 클래스를 구체적으로 지정하기 보다는
+  // 인터페이스를 지정함으로써
+  // 향후 다른 구현체로 교체하기 쉽도록 한다.
+  //
+  MemberService memberservice;
+
+  public MemberAddServlet(MemberService memberservice) {
+    this.memberservice = memberservice;
+  }
+
+  @Override
+  public void service(Scanner in, PrintStream out) throws Exception {
+    Member member = new Member();
+    member.setName(Prompt.getString(in, out, "이름? "));
+    member.setEmail(Prompt.getString(in, out, "이메일? "));
+    member.setPassword(Prompt.getString(in, out, "암호? "));
+    member.setPhoto(Prompt.getString(in, out, "사진? "));
+    member.setTel(Prompt.getString(in, out, "전화? "));
+    memberservice.add(member);
+    out.println("회원을 저장했습니다.");
+  }
+}
