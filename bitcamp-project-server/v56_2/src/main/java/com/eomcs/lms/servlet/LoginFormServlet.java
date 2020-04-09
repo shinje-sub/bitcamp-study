342d8ee7a86e4b931d14f8fe18158f0397395ab8
shinji-sub <wltjq2006@naver.com>
2020-04-09 10:29:43 +0900

    v56_2 - 서블릿을 이용하여 스프링 IoC 컨테이너 준비하기

--- bitcamp-project-server/v56_2/src/main/java/com/eomcs/lms/servlet/LoginFormServlet.java
+++ bitcamp-project-server/v56_2/src/main/java/com/eomcs/lms/servlet/LoginFormServlet.java
@@ -2,37 +2,36 @@ package com.eomcs.lms.servlet;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import javax.servlet.GenericServlet;
 import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/member/addForm")
-public class MemberAddFormServlet extends GenericServlet {
+@WebServlet("/auth/loginForm")
+public class LoginFormServlet extends HttpServlet {
   private static final long serialVersionUID = 1L;
 
   @Override
-  public void service(ServletRequest req, ServletResponse res)
+  protected void doGet(HttpServletRequest request, HttpServletResponse response)
       throws ServletException, IOException {
-    res.setContentType("text/html;charset=UTF-8");
-    PrintWriter out = res.getWriter();
+
+    response.setContentType("text/html;charset=UTF-8");
+    PrintWriter out = response.getWriter();
+
     try {
       out.println("<!DOCTYPE html>");
       out.println("<html>");
       out.println("<head>");
       out.println("<meta charset='UTF-8'>");
-      out.println("<title>회원 입력</title>");
+      out.println("<title>로그인</title>");
       out.println("</head>");
       out.println("<body>");
-      out.println("<h1>회원 입력</h1>");
-      out.println("<form action='add'>");
-      out.println("이름: <input name='name' type='text'><br>");
+      out.println("<h1>로그인</h1>");
+      out.println("<form action='login'>");
       out.println("이메일: <input name='email' type='email'><br>");
       out.println("암호: <input name='password' type='password'><br>");
-      out.println("사진: <input name='photo' type='text'><br>");
-      out.println("전화: <input name='tel' type='tel'><br>");
-      out.println("<button>제출</button>");
+      out.println("<button>로그인</button>");
       out.println("</form>");
       out.println("</body>");
       out.println("</html>");
