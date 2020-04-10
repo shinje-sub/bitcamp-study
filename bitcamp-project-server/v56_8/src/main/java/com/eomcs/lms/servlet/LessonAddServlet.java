b8873e5291611dc3645510be7294ea118036d007
shinji-sub <wltjq2006@naver.com>
2020-04-10 14:15:38 +0900

    v56_8 - 파일 업로드 기능 추가

--- bitcamp-project-server/v56_8/src/main/java/com/eomcs/lms/servlet/LessonAddServlet.java
+++ bitcamp-project-server/v56_8/src/main/java/com/eomcs/lms/servlet/LessonAddServlet.java
@@ -2,22 +2,19 @@ package com.eomcs.lms.servlet;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.UUID;
+import java.sql.Date;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.Part;
 import org.springframework.context.ApplicationContext;
-import com.eomcs.lms.domain.Member;
-import com.eomcs.lms.service.MemberService;
+import com.eomcs.lms.domain.Lesson;
+import com.eomcs.lms.service.LessonService;
 
-@WebServlet("/member/add")
-@MultipartConfig(maxFileSize = 10000000)
-public class MemberAddServlet extends HttpServlet {
+@WebServlet("/lesson/add")
+public class LessonAddServlet extends HttpServlet {
   private static final long serialVersionUID = 1L;
 
   @Override
@@ -29,13 +26,15 @@ public class MemberAddServlet extends HttpServlet {
 
       request.getRequestDispatcher("/header").include(request, response);
 
-      out.println("<h1>회원 입력</h1>");
-      out.println("<form action='add' method='post' enctype='multipart/form-data'>");
-      out.println("이름: <input name='name' type='text'><br>");
-      out.println("이메일: <input name='email' type='email'><br>");
-      out.println("암호: <input name='password' type='password'><br>");
-      out.println("사진: <input name='photo' type='file'><br>");
-      out.println("전화: <input name='tel' type='tel'><br>");
+      out.println("<h1>강의 입력</h1>");
+      out.println("<form action='add' method='post'>");
+      out.println("강의명: <input name='title' type='text'><br>");
+      out.println("내용:<br>");
+      out.println("<textarea name='description' rows='5' cols='60'></textarea><br>");
+      out.println("강의 시작일: <input name='startDate' type='date'><br>");
+      out.println("강의 종료일: <input name='endDate' type='date'><br>");
+      out.println("총 강의시간: <input name='totalHours' type='number'><br>");
+      out.println("일 강의시간: <input name='dayHours' type='number'><br>");
       out.println("<button>제출</button>");
       out.println("</form>");
 
@@ -57,27 +56,22 @@ public class MemberAddServlet extends HttpServlet {
       ServletContext servletContext = getServletContext();
       ApplicationContext iocContainer =
           (ApplicationContext) servletContext.getAttribute("iocContainer");
-      MemberService memberService = iocContainer.getBean(MemberService.class);
+      LessonService lessonService = iocContainer.getBean(LessonService.class);
 
-      Member member = new Member();
-      member.setName(request.getParameter("name"));
-      member.setEmail(request.getParameter("email"));
-      member.setPassword(request.getParameter("password"));
-      member.setTel(request.getParameter("tel"));
+      Lesson lesson = new Lesson();
+      lesson.setTitle(request.getParameter("title"));
+      lesson.setDescription(request.getParameter("description"));
+      lesson.setStartDate(Date.valueOf(request.getParameter("startDate")));
+      lesson.setEndDate(Date.valueOf(request.getParameter("endDate")));
+      lesson.setTotalHours(Integer.parseInt(request.getParameter("totalHours")));
+      lesson.setDayHours(Integer.parseInt(request.getParameter("dayHours")));
 
-      Part photoPart = request.getPart("photo");
-      if (photoPart.getSize() > 0) {
-        String dirPath = getServletContext().getRealPath("/upload/member");
-        String filename = UUID.randomUUID().toString();
-        photoPart.write(dirPath + "/" + filename);
-        member.setPhoto(filename);
-      }
-
-      if (memberService.add(member) > 0) {
+      if (lessonService.add(lesson) > 0) {
         response.sendRedirect("list");
       } else {
-        throw new Exception("회원을 추가할 수 없습니다.");
+        throw new Exception("수업을 추가할 수 없습니다.");
       }
+
     } catch (Exception e) {
       request.setAttribute("error", e);
       request.setAttribute("url", "list");
